35d6fa8ab12d48a6c05ffb3bf3ad6715b838c1d8
Chanrley Ferreira <35461240+chanrley@users.noreply.github.com>
2023-04-18 18:31:33 -0300

    alert(message) in Python working

---
+++ alert-in-python/testar.py
@@ -0,0 +1,22 @@
+# Python 3.x code
+# Imports
+import tkinter
+from tkinter import messagebox
+
+# This code is to hide the main tkinter window
+root = tkinter.Tk()
+root.withdraw()
+
+# Message Box
+messagebox.showinfo("Title", "Message")
+
+#######
+#Types#
+#######
+# showinfo()
+# showwarning()
+# showerror ()
+# askquestion()
+# askokcancel()
+# askyesno ()
+# askretrycancel ()
